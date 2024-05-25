d55b5647bb3bb197b3e5e0e467c5ff536bcaf898
Jiarui <1457866277@qq.com>
2024-05-25 18:22:31 +0800

    message 15: new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,18 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.models import Item
 
+class ListViewTst(TestCase):
+    def test_users_list_template(self):
+        response = self.client.get('/lists/the-new-page/')
+        self.assertTemplateUsed(response,'list.html')
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-new-page/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
@@ -25,12 +37,7 @@ class ItemModelTest(TestCase):
         self.assertEqual(second_saved_item.text, 'Item the second')
 class HomePageTest(TestCase):
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-        response=self.client.get('/')
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+
     def test_uses_home_template(self):
         response=self.client.get('/')
         self.assertTemplateUsed(response,'home.html')
@@ -45,7 +52,7 @@ class HomePageTest(TestCase):
 
         response=self.client.post('/',data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code,302)
-        self.assertEqual(response['location'],'/')
+        self.assertEqual(response['location'],'/lists/the-new-page/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
