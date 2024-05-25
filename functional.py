984d85fa1407c1a5dfe8468b0217659139e4f9b3
Jiarui <1457866277@qq.com>
2024-05-25 13:16:45 +0800

    message 2: The first functional test

---
+++ functional.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import unittest
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser=webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retireve_it_later(self):
+        self.assertIn('To-Do',self.browser.title),"browser title was:" +self.browser.title
+        self.fail('Finish the test!')
+
+if __name__=='__main__':
+    unittest.main()
